fd1a03d52dd7d7ab5b66fc49b564e9d73e3fa5d8
Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>
2020-09-19 22:07:15 -0400

    Revert "Git Commit Sync Latest 4.9-fixes from queue-4.9 linux stable."
    This reverts commit 73811b4bd9923fa0d0edf9716c19a999e67b4a91.

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -5485,9 +5485,9 @@ void read_extent_buffer(const struct extent_buffer *eb, void *dstv,
 	}
 }
 
-int read_extent_buffer_to_user_nofault(const struct extent_buffer *eb,
-				       void __user *dstv,
-				       unsigned long start, unsigned long len)
+int read_extent_buffer_to_user(const struct extent_buffer *eb,
+			       void __user *dstv,
+			       unsigned long start, unsigned long len)
 {
 	size_t cur;
 	size_t offset;
@@ -5508,7 +5508,7 @@ int read_extent_buffer_to_user_nofault(const struct extent_buffer *eb,
 
 		cur = min(len, (PAGE_SIZE - offset));
 		kaddr = page_address(page);
-		if (probe_user_write(dst, kaddr + offset, cur)) {
+		if (copy_to_user(dst, kaddr + offset, cur)) {
 			ret = -EFAULT;
 			break;
 		}
--- net/batman-adv/bridge_loop_avoidance.c
+++ net/batman-adv/bridge_loop_avoidance.c
@@ -451,10 +451,7 @@ static void batadv_bla_send_claim(struct batadv_priv *bat_priv, u8 *mac,
 			   skb->len + ETH_HLEN);
 	soft_iface->last_rx = jiffies;
 
-	if (in_interrupt())
-		netif_rx(skb);
-	else
-		netif_rx_ni(skb);
+	netif_rx(skb);
 out:
 	if (primary_if)
 		batadv_hardif_put(primary_if);
